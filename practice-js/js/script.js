46881bdbdb0fe9cecb175fa5facb4bb091e521f2
MaxLu <maxlu.com@gmail.com>
2024-02-01 17:59:09 +0800

    通用语言结构(运算符、比较等等)

--- practice-js/js/script.js
+++ practice-js/js/script.js
@@ -1,10 +1,12 @@
 // let x= "hello max";
 let s = 2;
 A();
-function A () {
+
+function A() {
     s = 4;
     B();
-    function B () {
+
+    function B() {
         console.log(s);
     }
 
@@ -27,7 +29,87 @@ if (x == undefined) {
 x = 5;
 if (x == undefined) {
     console.log("x is undefined");
-}
-else {
+} else {
     console.log("x is defined");
 }
+
+// 通用语言结构
+
+
+// 字符串的连接
+let string = "hello";
+string += " world";
+// string = string + " world";
+console.log(string + "!");
+
+//数学运算符的测试
+console.log((5 + 4) / 3);
+
+console.log(undefined / 5);
+// 返回 NaN ，NaN 是一个特殊的值，代表 "Not a Number"（不是一个数字）。
+// 当一个数学运算无法生成正常的数值时，就会返回 NaN。
+
+
+// equality
+
+let a = 4, b = 4;
+if (a == b) {
+    console.log("a=4 is equal to b=4");
+}
+
+// == 会自动转换类型，所以这里在比较中实际的字符串4会被转换成数字4，
+// 如果不想被转换可以使用 === 来比较，具体示例如下。
+
+// 会转换
+a = "4";
+if (a == b) {
+    console.log("b=4 is equal to a='4'");
+}
+
+// 不会转换
+if (a === b) {
+    console.log("b=4 is equal to a='4'");
+} else {
+    console.log("b=4 is NOT equal to a='4'");
+}
+
+// 布尔值的测试，用 or 运算符测试多个值，如果一个为真则返回 true，
+// 但在 js 中下面测试的都被视为 false，所以会执行 else语句块，所以会打印 "All false"。
+if (false || null || undefined || "" || 0 || NaN) {
+    console.log("This line won't ever execute");
+} else {
+    console.log("All false");
+}
+// 用 and 运算符测试多个值，如果一个为假则返回 false，
+if (true && "hello" && 1 && -1 && "false") {
+    console.log("All true");
+}
+// 函数 p 的返回值并不是我们期望的 {name: "max"}，
+// 而是 undefined。这是因为在 JavaScript 中，
+// return 语句后面的对象字面量必须和return 在同一行，
+// 否则 return 后面的内容会被忽略，函数就会返回 undefined。
+// function p()
+// {
+//     return
+//     {
+//         name: "max"
+//     };
+// }
+
+function q() {
+    return {
+        name: "Max"
+    };
+}
+
+// console.log(p());
+console.log(q());
+
+// for 循环
+// 定义一个变量 sum，初始化为 0
+let sum = 0;
+// 使用 for 循环从 0 加到 9，在每次循环中，将当前的迭代值i加到 sum 中
+for (let i = 0; i <10; i++) {
+    sum += i;
+}
+console.log("从 0 加到 9 的和是：" + sum);
